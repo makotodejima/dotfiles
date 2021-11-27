92a7bf1f1bae84fb8aaa02f6df4c54bdbc0c82e8
Makoto Dejima <makotodejima@gmail.com>
2021-11-27 21:12:35 +0100

    treesitter text object

--- .config/nvim/lua/mkd/tree-sitter.lua
+++ .config/nvim/lua/mkd/tree-sitter.lua
@@ -5,5 +5,29 @@ require"nvim-treesitter.configs".setup {
     enable = true,
     keymaps = {node_incremental = "ii", node_decremental = "II"}
   },
-  context_commentstring = {enable = true}
+  context_commentstring = {enable = true},
+  textobjects = {
+    select = {
+      enable = true,
+
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner"
+
+        -- Or you can define your own textobjects like this
+        -- ["iF"] = {
+        --   python = "(function_definition) @function",
+        --   cpp = "(function_definition) @function",
+        --   c = "(function_definition) @function",
+        --   java = "(method_declaration) @function"
+        -- }
+      }
+    }
+  }
 }
