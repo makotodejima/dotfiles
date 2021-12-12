e64f8b9a62b67b47ac44930523c28a0fc4216c94
Makoto Dejima <makotodejima@gmail.com>
2021-12-12 22:26:06 +0100

    turn off copy sync due to lagging

--- .config/nvim/lua/mkd/tmux.lua
+++ .config/nvim/lua/mkd/tmux.lua
@@ -2,7 +2,7 @@ require("tmux").setup({
   copy_sync = {
   -- enables copy sync and overwrites all register actions to
   -- sync registers *, +, unnamed, and 0 till 9 from tmux in advance
-      enable = true,
+      enable = false,
   },
   navigation = {
       enable_default_keybindings = true,
