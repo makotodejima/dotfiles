91cb7bf0c76ca58cbfe695496e63c60bb11f262b
Makoto Dejima <makotodejima@gmail.com>
2022-03-12 15:14:16 +0100

    lualine update

--- .config/nvim/lua/mkd/lualine.lua
+++ .config/nvim/lua/mkd/lualine.lua
@@ -1,30 +1,52 @@
-local function lsp_info()
-  local warnings = vim.lsp.diagnostic.get_count(0, "Warning")
-  local errors = vim.lsp.diagnostic.get_count(0, "Error")
-  local hints = vim.lsp.diagnostic.get_count(0, "Hint")
-  return string.format("LSP: H %d W %d E %d", hints, warnings, errors)
-end
-
 local function path()
   local sep = "/"
   local subs = {}
   for str in string.gmatch(vim.fn.expand('%'), "([^" .. sep .. "]+)") do table.insert(subs, str) end
 
-  local name = ""
+  local str = ""
+  if #subs < 3 then
+    for i, level in ipairs(subs) do
+      local parent = str
+      if i == 1 then
+        str = level
+      else
+        str = parent .. sep .. level
+      end
+    end
+    return str
+  end
+
   for i, level in ipairs(subs) do
-    local parent = name
+    local parent = str
     if i == 1 then
-      name = string.sub(level, 0, 2)
+      str = string.sub(level, 0, 2)
     elseif i < #subs - 1 then
-      name = parent .. "/" .. string.sub(level, 0, 2)
+      str = parent .. sep .. string.sub(level, 0, 2)
     else
-      name = parent .. "/" .. level
+      str = parent .. sep .. level
     end
   end
-  return name
+  return str
 end
 
 local components = require("mkd.lsp-status")
+
+local diagnostics = {
+  'diagnostics',
+  sources = {'nvim_lsp'},
+  sections = {'error', 'warn', 'info', 'hint'},
+  symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'},
+  colored = true,
+  update_in_insert = false,
+  always_visible = false
+}
+
+local diff = {
+  'diff',
+  colored = true,
+  diff_color = {added = {fg = '#32A0B4'}, modified = {fg = '#E6B450'}, removed = {fg = '#B40000'}},
+  symbols = {added = '+', modified = '~', removed = '-'}
+}
 
 require'lualine'.setup {
   options = {
@@ -37,18 +59,7 @@ require'lualine'.setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', path},
-    lualine_c = {
-      {
-        'diff',
-        colored = true,
-        diff_color = {
-          added = {fg = '#32A0B4'},
-          modified = {fg = '#E6B450'},
-          removed = {fg = '#B40000'}
-        },
-        symbols = {added = '+', modified = '~', removed = '-'}
-      }, lsp_info, components.lsp_status
-    },
+    lualine_c = {diff, diagnostics, components.lsp_status},
     lualine_x = {'encoding', 'fileformat', 'filetype', 'filesize'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
