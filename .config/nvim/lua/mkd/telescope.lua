a9282ed7ac5df74aebcf04f919fdf03bb8456a09
Makoto Dejima <makotodejima@gmail.com>
2022-03-12 15:14:49 +0100

    buffer sorting

--- .config/nvim/lua/mkd/telescope.lua
+++ .config/nvim/lua/mkd/telescope.lua
@@ -11,7 +11,7 @@ require("telescope").setup({
   pickers = {
     buffers = {
       ignore_current_buffer = true,
-      sort_lastused = true,
+      sort_mru = true,
       mappings = {i = {["<c-d>"] = "delete_buffer"}}
     }
   }
