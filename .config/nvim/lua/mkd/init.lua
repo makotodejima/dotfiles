b7040f7b2a4e73e67d703c0d1a155156305cfe48
Makoto Dejima <makotodejima@gmail.com>
2022-05-22 18:00:43 +0200

    move lua setups

--- .config/nvim/lua/mkd/init.lua
+++ .config/nvim/lua/mkd/init.lua
@@ -2,9 +2,16 @@ require("mkd.diffview")
 require("mkd.efm")
 require("mkd.lsp")
 require("mkd.lualine")
+require("mkd.null-ls")
 require("mkd.telescope")
 require("mkd.tmux")
 require("mkd.tree-sitter")
+require("mkd.dap")
+
+require('neoclip').setup()
+require('marks').setup {}
+require("fidget").setup {}
+require("nvim-gps").setup {disable_icons = true}
 
 P = function(v)
   print(vim.inspect(v))
