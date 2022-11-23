58cd5f08ba3ff99a608868e5b697d9ab01eebe5e
Makoto Dejima <makotodejima@gmail.com>
2022-11-24 00:07:17 +0200

    linters only run by null-ls

--- nvim/.config/nvim/lua/mkd/null-ls/init.lua
+++ nvim/.config/nvim/lua/mkd/null-ls/init.lua
@@ -1,21 +1,30 @@
-local null_ls = require('null-ls')
+local null_ls = require "null-ls"
 
-null_ls.setup({
+local opt = { noremap = true, silent = false }
+vim.keymap.set("n", "<leader><space>", ":lua vim.lsp.buf.format()<CR>", opt)
+vim.keymap.set("v", "<leader>ss", ":lua require 'mkd.null-ls.cspell_util'.add_selection()<CR>", opt)
+
+null_ls.setup {
   -- debug = true,
   sources = {
     -- null_ls.builtins.diagnostics.pylint,
-    null_ls.builtins.formatting.terraform_fmt, null_ls.builtins.formatting.jq,
-    null_ls.builtins.formatting.lua_format.with({
-      extra_args = {"--column-limit=100", "--indent-width=2"}
-    }), null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.prettier.with({prefer_local = "node_modules/.bin", timeout = 30000}),
-    null_ls.builtins.diagnostics.cspell.with({
+    null_ls.builtins.formatting.terraform_fmt,
+    null_ls.builtins.formatting.jq,
+    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.formatting.eslint_d,
+    null_ls.builtins.formatting.rustfmt,
+    null_ls.builtins.formatting.pg_format,
+    null_ls.builtins.formatting.stylua.with {
+      extra_args = { "--config-path", vim.fn.expand "~/.config/nvim/lua/mkd/null-ls/stylua.toml" },
+    },
+    null_ls.builtins.formatting.prettier.with { prefer_local = "node_modules/.bin", timeout = 30000 },
+    null_ls.builtins.diagnostics.cspell.with {
       method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
-      disabled_filetypes = {"vim", "lua", "netrw"},
-      extra_args = {"--config", vim.fn.expand("~/.config/nvim/lua/mkd/null-ls/cspell.json")},
+      disabled_filetypes = { "vim", "lua", "netrw" },
+      extra_args = { "--config", vim.fn.expand "~/.config/nvim/lua/mkd/null-ls/cspell.json" },
       diagnostics_postprocess = function(diagnostic)
         diagnostic.severity = vim.diagnostic.severity.INFO
-      end
-    })
-  }
-})
+      end,
+    },
+  },
+}
