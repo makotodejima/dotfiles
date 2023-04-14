1d2074b7296ff028e74f7bac486193d9406c4973
Makoto Dejima <makotodejima@gmail.com>
2023-04-14 10:42:48 +0200

    remove unnecessary color set

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -5,10 +5,8 @@ function Color()
     vim.api.nvim_set_hl(0, thing, opts)
   end
 
-  -- hl("CursorLine", { bg = "none" })
   hl("Sneak", { fg = "#002335", bg = "#FFD5D1" })
   hl("MatchParen", { bold = true, underline = true })
-  hl("netrwDir", { fg = "#b2c6d4" })
 end
 
 Color()
