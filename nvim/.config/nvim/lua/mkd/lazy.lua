a9ee9f00903d99a2cee566646db30a2ed84eb15b
Makoto Dejima <makotodejima@gmail.com>
2023-09-28 12:40:48 +0200

    add filetypes to indent-blankline

--- nvim/.config/nvim/lua/mkd/lazy.lua
+++ nvim/.config/nvim/lua/mkd/lazy.lua
@@ -131,7 +131,7 @@ require("lazy").setup({
     "lukas-reineke/indent-blankline.nvim",
     config = function()
       require("indent_blankline").setup {
-        filetype = { "python" },
+        filetype = { "python", "yaml", "toml" },
       }
     end,
   },
