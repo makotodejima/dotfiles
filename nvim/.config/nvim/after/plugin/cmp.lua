73c4cbd1516af9e1fbe909d38bd9bac6ebe5ae86
Makoto Dejima <makotodejima@gmail.com>
2023-12-26 22:24:23 +0900

    disable cmp for big file

--- nvim/.config/nvim/after/plugin/cmp.lua
+++ nvim/.config/nvim/after/plugin/cmp.lua
@@ -74,3 +74,27 @@ cmp.setup.cmdline(":", {
     { { name = "cmdline_history", max_item_count = 5 }, { name = "cmdline", max_item_count = 10 } }
   ),
 })
+
+-- If file is huge i want to disable cmp
+vim.api.nvim_create_autocmd("BufReadPre", {
+  callback = function(t)
+    if bufIsBig(t.buf) then
+      cmp.setup.buffer {
+        enabled = function()
+          return false
+        end,
+        -- sources = {},
+      }
+    end
+  end,
+})
+
+bufIsBig = function(bufnr)
+  local max_filesize = 100 * 1024 -- 100 KB
+  local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
+  if ok and stats and stats.size > max_filesize then
+    return true
+  else
+    return false
+  end
+end
