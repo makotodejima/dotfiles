52bfa4e464b8ca1e971caf476b7b098318511497
Makoto Dejima <makotodejima@gmail.com>
2024-05-21 10:46:14 +0200

    fix user commands

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -38,17 +38,54 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
+local function slice_string_at_whitespace(input_string)
+  -- slices the input string at the first occurrence of whitespaces
+  local start_pos, _ = string.find(input_string, "%s%s%s+")
+  if start_pos then
+    return string.sub(input_string, 1, start_pos - 1)
+  else
+    return input_string
+  end
+end
+
 local function open_with_preview()
-  -- Check if the current buffer is netrw
-  if vim.bo.filetype == "netrw" then
-    local line = vim.fn.getline "."
-    local filename = line:match "^[^%s]+"
-    local file_under_cursor = vim.fn.expand "%:p" .. filename
+  if vim.bo.filetype ~= "netrw" then
+    print "Open with preview from netrw only."
+    return
+  end
+
+  local line = vim.fn.getline "."
+  local filename = slice_string_at_whitespace(line:match "^%s*(.-)%s*$")
+  local file_under_cursor = vim.fn.expand "%:p:h" .. "/" .. filename
+  local extension = file_under_cursor:match "^.+(%..+)$"
+  if
+    extension == ".png"
+    or extension == ".jpg"
+    or extension == ".psd"
+    or extension == ".tiff"
+    or extension == ".pdf"
+  then
     os.execute('open -a Preview "' .. file_under_cursor .. '"')
   else
-    local file_under_cursor = vim.fn.expand "<cfile>"
-    os.execute('open -a Preview "' .. file_under_cursor .. '"')
+    P "File extension not supported."
   end
 end
 
 vim.api.nvim_create_user_command("OpenPreview", open_with_preview, {})
+
+local function tsNodeOnBuffer()
+  local current_file_path = vim.api.nvim_buf_get_name(0)
+
+  if current_file_path == "" then
+    print "No file is currently open in the buffer."
+    return
+  end
+
+  local output = vim.fn.system("ts-node " .. current_file_path)
+  vim.cmd "split"
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(output, "\n"))
+  vim.api.nvim_set_current_buf(buf)
+end
+
+vim.api.nvim_create_user_command("TSNode", tsNodeOnBuffer, {})
