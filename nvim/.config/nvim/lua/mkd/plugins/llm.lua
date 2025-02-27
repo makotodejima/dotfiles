430723ede456ebdf4aadc752d762fcef5ce30625
Makoto Dejima <makoto@legalos.io>
2025-02-27 20:51:28 +0100

    add git commit message generation command using gpt-4o

--- nvim/.config/nvim/lua/mkd/plugins/llm.lua
+++ nvim/.config/nvim/lua/mkd/plugins/llm.lua
@@ -16,6 +16,27 @@ return {
   end,
   {
     "madox2/vim-ai",
-    cmd = { "AI", "AIEdit", "AIChat", "AIE", "AIC" },
+    cmd = { "AI", "AIEdit", "AIChat", "AIE", "AIC", "CM" },
+    config = function()
+      local function git_commit_message()
+        local diff = vim.fn.system "git --no-pager diff --staged"
+        local prompt = "generate a concise commit message (all lower case) from the diff below:\n\n<diff>"
+          .. diff
+          .. "</diff>"
+
+        local config = {
+          engine = "chat",
+          options = {
+            model = "gpt-4o",
+            -- initial_prompt = ">>> system\nyou are a code assistant",
+            temperature = 1,
+          },
+        }
+
+        vim.call("vim_ai#AIRun", false, config, prompt)
+      end
+
+      vim.api.nvim_create_user_command("CM", git_commit_message, {})
+    end,
   },
 }
