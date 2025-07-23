6e8c205fc538eff4d78b2ba4bc33e50c3807fd0b
Makoto Dejima <makoto@flank.ai>
2025-07-23 23:13:52 +0200

    dadbod

---
+++ nvim/.config/nvim/lua/mkd/plugins/db.lua
@@ -0,0 +1,19 @@
+return {
+  "kristijanhusak/vim-dadbod-ui",
+  dependencies = {
+    { "tpope/vim-dadbod", lazy = true },
+    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+  },
+  cmd = {
+    "DBUI",
+    "DBUIToggle",
+    "DBUIAddConnection",
+    "DBUIFindBuffer",
+  },
+  init = function()
+    vim.g.dbs = {
+      { name = "dev", url = "mongodb://localhost:3001/meteor" },
+    }
+    vim.g.db_ui_use_nerd_fonts = 1
+  end,
+}
