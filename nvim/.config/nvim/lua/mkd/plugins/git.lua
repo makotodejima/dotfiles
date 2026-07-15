8e561d1121d0d1ed79a25fbe0f903daa67b93837
Makoto Dejima <makotodejima@gmail.com>
2026-07-15 12:27:22 +0200

    migrate git plugins to vim.pack

--- nvim/.config/nvim/lua/mkd/plugins/git.lua
+++ nvim/.config/nvim/lua/mkd/plugins/git.lua
@@ -5,96 +5,98 @@
 -- `:Gitsigns change_base origin/main all`
 -- `:Gitsigns setqflist all` or `:G difftool origin/main`
 
-return {
-  "tpope/vim-fugitive",
-  {
-    "tpope/vim-rhubarb",
-    event = "CmdlineEnter",
+vim.pack.add({
+  "https://github.com/tpope/vim-fugitive",
+  "https://github.com/tpope/vim-rhubarb",
+  "https://github.com/lewis6991/gitsigns.nvim",
+}, {
+  confirm = false,
+  -- Temporary while lazy.nvim still owns startup: it sources plugin scripts
+  -- from these runtime paths. Change this to true at the final cutover.
+  load = false,
+})
+
+require("gitsigns").setup({
+  signs = {
+    add = { text = "+" },
+    change = { text = "~" },
+    delete = { text = "_" },
+    topdelete = { text = "‾" },
+    changedelete = { text = "~" },
   },
-  {
-    "lewis6991/gitsigns.nvim",
-    lazy = false,
-    config = function()
-      require("gitsigns").setup({
-        signs = {
-          add = { text = "+" },
-          change = { text = "~" },
-          delete = { text = "_" },
-          topdelete = { text = "‾" },
-          changedelete = { text = "~" },
-        },
-        max_file_length = 20000,
-        on_attach = function(bufnr)
-          local gitsigns = require("gitsigns")
+  max_file_length = 20000,
+  on_attach = function(bufnr)
+    local gitsigns = require("gitsigns")
 
-          local function map(mode, l, r, opts)
-            opts = opts or {}
-            opts.buffer = bufnr
-            vim.keymap.set(mode, l, r, opts)
-          end
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
 
-          -- Navigation
-          map("n", "]c", function()
-            if vim.wo.diff then
-              vim.cmd.normal({ "]c", bang = true })
-            else
-              gitsigns.nav_hunk("next")
-            end
-          end)
+    -- Navigation
+    map("n", "]c", function()
+      if vim.wo.diff then
+        vim.cmd.normal({ "]c", bang = true })
+      else
+        gitsigns.nav_hunk("next")
+      end
+    end)
 
-          map("n", "[c", function()
-            if vim.wo.diff then
-              vim.cmd.normal({ "[c", bang = true })
-            else
-              gitsigns.nav_hunk("prev")
-            end
-          end)
+    map("n", "[c", function()
+      if vim.wo.diff then
+        vim.cmd.normal({ "[c", bang = true })
+      else
+        gitsigns.nav_hunk("prev")
+      end
+    end)
 
-          -- Actions
-          map("n", "<leader>hs", gitsigns.stage_hunk)
-          map("n", "<leader>hr", gitsigns.reset_hunk)
+    -- Actions
+    map("n", "<leader>hs", gitsigns.stage_hunk)
+    map("n", "<leader>hr", gitsigns.reset_hunk)
 
-          map("v", "<leader>hs", function()
-            gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-          end)
+    map("v", "<leader>hs", function()
+      gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+    end)
 
-          map("v", "<leader>hr", function()
-            gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-          end)
+    map("v", "<leader>hr", function()
+      gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+    end)
 
-          map("n", "<leader>hS", gitsigns.stage_buffer)
-          map("n", "<leader>hR", gitsigns.reset_buffer)
-          map("n", "<leader>hp", gitsigns.preview_hunk)
-          map("n", "<leader>hi", gitsigns.preview_hunk_inline)
+    map("n", "<leader>hS", gitsigns.stage_buffer)
+    map("n", "<leader>hR", gitsigns.reset_buffer)
+    map("n", "<leader>hp", gitsigns.preview_hunk)
+    map("n", "<leader>hi", gitsigns.preview_hunk_inline)
 
-          map("n", "<leader>hb", function()
-            gitsigns.blame_line({ full = true })
-          end)
+    map("n", "<leader>hb", function()
+      gitsigns.blame_line({ full = true })
+    end)
 
-          map("n", "<leader>hd", gitsigns.diffthis)
+    map("n", "<leader>hd", gitsigns.diffthis)
 
-          map("n", "<leader>hD", function()
-            gitsigns.diffthis("~")
-          end)
+    map("n", "<leader>hD", function()
+      gitsigns.diffthis("~")
+    end)
 
-          map("n", "<leader>hQ", function()
-            gitsigns.setqflist("all")
-          end)
-          map("n", "<leader>hq", gitsigns.setqflist)
+    map("n", "<leader>hQ", function()
+      gitsigns.setqflist("all")
+    end)
+    map("n", "<leader>hq", gitsigns.setqflist)
 
-          -- Toggles
-          map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
-          map("n", "<leader>tw", gitsigns.toggle_word_diff)
+    -- Toggles
+    map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
+    map("n", "<leader>tw", gitsigns.toggle_word_diff)
 
-          -- Text object
-          map({ "o", "x" }, "ih", gitsigns.select_hunk)
-        end,
-      })
+    -- Text object
+    map({ "o", "x" }, "ih", gitsigns.select_hunk)
+  end,
+})
 
-      vim.keymap.set("n", "<leader>gs", ":G<CR>", { noremap = true, silent = false })
-      vim.keymap.set("n", "<leader>gh", ":diffget //2", { noremap = true, silent = false })
-      vim.keymap.set("n", "<leader>gl", ":diffget //3", { noremap = true, silent = false })
-      vim.keymap.set("n", "<leader>gw", ":Gwrite<CR>", { noremap = true, silent = false })
-    end,
-  },
-}
+vim.keymap.set("n", "<leader>gs", ":G<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>gh", ":diffget //2", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>gl", ":diffget //3", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>gw", ":Gwrite<CR>", { noremap = true, silent = false })
+
+-- lazy.nvim expects every imported module to return a table of specs. The
+-- plugins above are now owned by vim.pack, so this module contributes none.
+return {}
