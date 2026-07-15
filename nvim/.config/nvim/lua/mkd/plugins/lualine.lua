58e2361dda042e04b2fb3a7e8462524a06d2dd16
Makoto Dejima <makotodejima@gmail.com>
2026-07-15 12:38:41 +0200

    migrate lualine to vim.pack

--- nvim/.config/nvim/lua/mkd/plugins/lualine.lua
+++ nvim/.config/nvim/lua/mkd/plugins/lualine.lua
@@ -6,12 +6,11 @@ local function worktree()
   return ("[" .. wt .. "]")
 end
 
---- @param trunc_width number trunctates component when screen width is less then trunc_width
+--- @param trunc_width number truncates component when screen width is less then trunc_width
 --- @param trunc_len number truncates component to trunc_len number of chars
 --- @param hide_width number hides component when window width is smaller then hide_width
 --- @param no_ellipsis boolean whether to disable adding '...' at end after truncation
 --- return function that can format the component accordingly
-
 local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
   return function(str)
     local win_width = vim.fn.winwidth(0)
@@ -24,65 +23,72 @@ local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
   end
 end
 
-return {
-  "hoob3rt/lualine.nvim",
-  event = "VeryLazy",
-  config = function()
-    local lualine = require("lualine")
+vim.pack.add({
+  "https://github.com/nvim-lualine/lualine.nvim",
+}, {
+  confirm = false,
+  -- Temporary while lazy.nvim still owns startup: it sources plugin scripts
+  -- from these runtime paths. Change this to true at the final cutover.
+  load = false,
+})
 
-    local diagnostics = {
-      "diagnostics",
-      sources = { "nvim_lsp" },
-      sections = { "error", "warn", "info", "hint" },
-      symbols = { error = "E", warn = "W", info = "I", hint = "H" },
-      colored = true,
-      update_in_insert = false,
-      always_visible = true,
-    }
+local lualine = require("lualine")
 
-    local function diff_source()
-      local gitsigns = vim.b.gitsigns_status_dict
-      if gitsigns then
-        return { added = gitsigns.added, modified = gitsigns.changed, removed = gitsigns.removed }
-      end
-    end
-
-    local diff = {
-      "diff",
-      source = diff_source,
-      colored = true,
-      diff_color = { added = { fg = "#32A0B4" }, modified = { fg = "#E6B450" }, removed = { fg = "#B40000" } },
-      symbols = { added = "+", modified = "~", removed = "-" },
-    }
-
-    lualine.setup({
-      options = {
-        icons_enabled = false,
-        theme = "auto",
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { worktree, { "branch", fmt = trunc(180, 15, 80) }, { "filename", path = 4 } },
-        lualine_c = { diff, diagnostics },
-        lualine_x = {
-          { "encoding", fmt = trunc(100, 50, 120) },
-          { "fileformat", fmt = trunc(100, 50, 120) },
-          { "filetype", fmt = trunc(100, 50, 120) },
-          { "filesize", fmt = trunc(100, 50, 120) },
-        },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "filetype", "location" },
-        lualine_y = {},
-        lualine_z = {},
-      },
-    })
-  end,
+local diagnostics = {
+  "diagnostics",
+  sources = { "nvim_lsp" },
+  sections = { "error", "warn", "info", "hint" },
+  symbols = { error = "E", warn = "W", info = "I", hint = "H" },
+  colored = true,
+  update_in_insert = false,
+  always_visible = true,
 }
+
+local function diff_source()
+  local gitsigns = vim.b.gitsigns_status_dict
+  if gitsigns then
+    return { added = gitsigns.added, modified = gitsigns.changed, removed = gitsigns.removed }
+  end
+end
+
+local diff = {
+  "diff",
+  source = diff_source,
+  colored = true,
+  diff_color = { added = { fg = "#32A0B4" }, modified = { fg = "#E6B450" }, removed = { fg = "#B40000" } },
+  symbols = { added = "+", modified = "~", removed = "-" },
+}
+
+lualine.setup({
+  options = {
+    icons_enabled = false,
+    theme = "auto",
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+  },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { worktree, { "branch", fmt = trunc(180, 15, 80) }, { "filename", path = 4 } },
+    lualine_c = { diff, diagnostics },
+    lualine_x = {
+      { "encoding", fmt = trunc(100, 50, 120) },
+      { "fileformat", fmt = trunc(100, 50, 120) },
+      { "filetype", fmt = trunc(100, 50, 120) },
+      { "filesize", fmt = trunc(100, 50, 120) },
+    },
+    lualine_y = { "progress" },
+    lualine_z = { "location" },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { "filename" },
+    lualine_x = { "filetype", "location" },
+    lualine_y = {},
+    lualine_z = {},
+  },
+})
+
+-- lazy.nvim expects every imported module to return a table of specs. This
+-- plugin is now owned by vim.pack, so this module contributes none.
+return {}
